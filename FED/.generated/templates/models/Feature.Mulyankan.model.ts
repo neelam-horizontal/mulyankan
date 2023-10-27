6f72fec1f53b572a64d25fb26e970bf4459d69d4
Garima Thakore <gthakore@horizontal.com>
2023-10-27 16:45:05 +0530

    config changes

---
+++ FED/.generated/templates/models/Feature.Mulyankan.model.ts
@@ -0,0 +1,10 @@
+// Auto-generated xWrap item wrappers for Feature.Mulyankan configuration
+using global::Sitecore.Data;
+using global::Sitecore.Data.Items;
+using global::Sitecore.Mvc.Presentation;
+using global::System.CodeDom.Compiler;
+using global::Xwrap;
+using global::Xwrap.FieldWrappers.Abstractions;
+using global::Xwrap.Mvc.RenderingParameters;
+
+
