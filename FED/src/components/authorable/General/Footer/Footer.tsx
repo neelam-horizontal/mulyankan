a804732f456a0338a96cb102767bcebff778cab5
Garima Thakore <gthakore@horizontal.com>
2023-10-28 16:08:05 +0530

    changes

--- FED/src/components/authorable/General/Footer/Footer.tsx
+++ FED/src/components/authorable/General/Footer/Footer.tsx
@@ -1,17 +1,8 @@
-// import { Field, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
-import { Field } from '@sitecore-jss/sitecore-jss-nextjs';
+
 import { Mulyankan } from '.generated/templates/models/Feature.Mulyankan.model';
 
-// Ideally, all this is from generated Typescript code from Sitecore and we're not manually defining types.
-interface Fields {
-    businessDescription: Field<string>;
-    businessTitle: Field<string>;
-    copyRightText:Field<string>;
-
-}
 
 export type FooterProps = Mulyankan.DataTemplates.Fields.Footer & {
-    theme: string;
     fields: Mulyankan.DataTemplates.Fields.Footer;
   };
 
@@ -21,8 +12,42 @@ const Footer = ({ fields }: FooterProps): JSX.Element => {
 
   return (
     <> <footer>
-        {fields.businessDescription.value}
-        </footer><div></div>
+       <div className="container-fluid bg-turquoise-dark  mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
+        <div className="container py-5">
+            <div className="row g-5">
+                <div className="col-lg-3 col-md-6">
+                    <h4 className="mb-4 text-turquoise">{fields.officeTitle.value}</h4>
+                    <p className="mb-2"><i className="fa fa-map-marker-alt me-3"></i>{fields.officeAddress.value}</p>
+                    <p className="mb-2"><i className="fa fa-phone-alt me-3"></i>{fields.officeNumber.value}</p>
+                    <p className="mb-2"><i className="fa fa-envelope me-3"></i>{fields.officeEmail.value}</p>
+                    <div className="d-flex pt-3">
+                        <a className="btn btn-square btn-light rounded-circle me-2" href=""><i className="fab fa-twitter"></i></a>
+                        <a className="btn btn-square btn-light rounded-circle me-2" href=""><i className="fab fa-facebook-f"></i></a>
+                        <a className="btn btn-square btn-light rounded-circle me-2" href=""><i className="fab fa-youtube"></i></a>
+                        <a className="btn btn-square btn-light rounded-circle me-2" href=""><i className="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6">
+                    <h4 className="text-white mb-4">Quick Links</h4>
+                    <a className="btn btn-link" href="">About Us</a>
+                    <a className="btn btn-link" href="">Contact Us</a>
+                    <a className="btn btn-link" href="">Our Services</a>
+                    <a className="btn btn-link" href="">Terms &amp; Condition</a>
+                    <a className="btn btn-link" href="">Support</a>
+                </div>
+                <div className="col-lg-3 col-md-6">
+                    <h4 className="text-white mb-4">Business Hours</h4>
+                    <p className="mb-1">Monday - Friday</p>
+                    <h6 className="text-light">09:00 am - 07:00 pm</h6>
+                    <p className="mb-1">Saturday</p>
+                    <h6 className="text-light">09:00 am - 12:00 pm</h6>
+                    <p className="mb-1">Sunday</p>
+                    <h6 className="text-light">Closed</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+        </footer>
     </>
  
   );
