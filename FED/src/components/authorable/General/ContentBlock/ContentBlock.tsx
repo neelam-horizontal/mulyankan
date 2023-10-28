5bbfb36563150a9ca7346cb4f33ba669f8d841ea
Vicky0939 <rohitsonawane0939@gmail.com>
2023-10-28 17:26:47 +0530

    changes

--- FED/src/components/authorable/General/ContentBlock/ContentBlock.tsx
+++ FED/src/components/authorable/General/ContentBlock/ContentBlock.tsx
@@ -3,6 +3,7 @@ import { Mulyankan } from '.generated/templates/models/Feature.Mulyankan.model';
 
 // Local
 import RichTextA11yWrapper from 'components/helpers/RichTextA11yWrapper/RichTextA11yWrapper';
+import { findKey } from 'lodash';
 
 // // Ideally, all this is from generated Typescript code from Sitecore and we're not manually defining types.
 // interface Fields {
@@ -25,10 +26,15 @@ const ContentBlock = ({ fields }: ContentBlockProps): JSX.Element => {
       data-component="authorable/general/contentblock"
       data-testid="contentblock"
     >
-      {/* <p className="font-bold">ContentBlocksss</p>   */}
-      <p className="fs-5 fw-medium mb-1 text-center  text-turquoise-dark text-yellow-700">{fields.eyebrow.value}</p>
-      <h1>{fields.title.value}</h1>
-      <RichTextA11yWrapper data-testid="contentblock" field={fields.description} editable />
+      <p className='fs-5 fw-medium mb-1 text-center  text-turquoise-dark'>{fields.eyebrow.value}</p>
+      <h1 className='display-5 mb-4'>{fields.title.value}</h1>
+      <RichTextA11yWrapper field={fields.description} editable />
+      <a className="d-inline-flex align-items-center rounded overflow-hidden border border-primary">
+                        <span className="btn-lg-square bg-primary">
+                            <i className="fa fa-phone-alt text-white"></i>
+                        </span>
+                        <span className="fs-5 fw-medium mx-4">{fields.linkTitle.value}</span>
+                    </a>
     </div>
   );
 };
