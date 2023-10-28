7714889425859418212c132d0d352b55fd9bce8d
Garima Thakore <gthakore@horizontal.com>
2023-10-28 17:08:28 +0530

    changes

--- FED/src/components/authorable/General/Footer/Footer.tsx
+++ FED/src/components/authorable/General/Footer/Footer.tsx
@@ -50,8 +50,6 @@ const Footer = ({ fields }: FooterProps): JSX.Element => {
               <div className="col-span-3">
                 <h4 className="text-white mb-4">{fields.linksTitle.value}</h4>
                 {fields.links?.map((list1Item, index) => {
-                  console.log(list1Item.fields.linkTitle.value);
-
                   return (<div>
                     <Link
                       href={list1Item.url}
@@ -68,12 +66,6 @@ const Footer = ({ fields }: FooterProps): JSX.Element => {
               </div>
               <div className="col-span-3">
                  <h4 className="text-white mb-4">{fields.businessTitle.value}</h4>
-                {/* <p className="mb-1">Monday - Friday</p>
-                <h6 className="text-light">09:00 am - 07:00 pm</h6>
-                <p className="mb-1">Saturday</p>
-                <h6 className="text-light">09:00 am - 12:00 pm</h6>
-                <p className="mb-1">Sunday</p>
-                <h6 className="text-light">Closed</h6>  */}
                 <RichTextA11yWrapper field={fields.businessDescription}></RichTextA11yWrapper>
                
               </div>
