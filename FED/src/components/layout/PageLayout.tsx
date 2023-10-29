8b29c6fae5b265e2a663526816876cea04019d13
Nisheesh Kulshreshtha <con_nkulshreshtha@plancanada.ca>
2023-10-29 14:09:22 +0530

    grid updates

--- FED/src/components/layout/PageLayout.tsx
+++ FED/src/components/layout/PageLayout.tsx
@@ -50,7 +50,7 @@ const PageLayout = ({ layoutData }: LayoutProps): JSX.Element => {
         <header>
           <div id="header">{route && <Placeholder name="headless-header" rendering={route} />}</div>
         </header>
-        <main>
+        <main className="mt-10">
           <div id="content">{route && <Placeholder name="headless-main" rendering={route} />}</div>
         </main>
         <footer>
